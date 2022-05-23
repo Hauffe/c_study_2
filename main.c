7840313c981a4cc99d0d3e56ae43c89613116ae7
Alexandre <alex_hauffe@hotmail.com>
2022-05-22 22:12:38 -0300

    add: adding cool matrix in c link

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #include "exercicios/exercicio_3.h"
 #include "exercicios/exercicio_4.h"
 
+
+//https://www.inf.ufpr.br/roberto/ci067/14_alocmat.html
 int main() {
     printf("Hello, World!\n");
     exercicio_4();
